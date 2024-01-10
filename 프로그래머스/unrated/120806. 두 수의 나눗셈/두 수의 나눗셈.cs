0a5ceb0ce22e2357229ca278cb867fc9d8df9bc1
E.Clone <aa01092308481@gmail.com>
2024-01-11 02:51:29 +0900

    [level unrated] Title: 두 수의 나눗셈, Time: 0.13 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120806. 두 수의 나눗셈/두 수의 나눗셈.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        answer = (int)((float)num1/(float)num2*1000);
+        return answer;
+    }
+}
