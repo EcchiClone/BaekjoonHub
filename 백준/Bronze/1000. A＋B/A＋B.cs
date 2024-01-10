731697be10a721d62ef0c03fec681c2a6e64bcc4
E.Clone <aa01092308481@gmail.com>
2024-01-11 01:55:22 +0900

    [Bronze V] Title: A+B, Time: 64 ms, Memory: 5504 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.cs
+++ 백준/Bronze/1000. A＋B/A＋B.cs
@@ -5,9 +5,7 @@ namespace BOJ {
         static void Main() {
             string s = Console.ReadLine();
             string[] ss = s.Split();
-            int a = int.Parse(ss[0]);
-            int b = int.Parse(ss[1]);
-            Console.WriteLine(a+b);
+            Console.WriteLine(int.Parse(ss[0])+int.Parse(ss[1]));
         }
     }
 }
