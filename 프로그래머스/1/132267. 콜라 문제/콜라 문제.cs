7a5fdbcc8bd2eef44775ef1d165127b064e1fed9
E.Clone <aa01092308481@gmail.com>
2024-01-16 22:49:29 +0900

    [level 1] Title: 콜라 문제, Time: 0.13 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        int d = a - b;
+        int n2 = n - b;
+        return answer = n2/d * b;
+    }
+}
